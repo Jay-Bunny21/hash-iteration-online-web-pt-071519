64370d9423e9973906809c51e59c2705e38775de
Jinai Franklin <jinai.franklin@yahoo.com>
2019-08-01 04:09:37 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -12,6 +12,6 @@ end
 
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
-    if age == age < 10 
+    if age == age < 10 &&
     puts"Happy Birthday #{kids_name}! You are now #{age} years old! You are too old for this."
   end
